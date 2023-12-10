a237e4b12f7396025d52b5f35773bffb909ff54d
entuenedufard <entuenedufard@gmail.com>
2023-12-10 21:36:32 +0100

    Refactor text expansion logic in TextModule.vue - that works

--- app.vue
+++ app.vue
@@ -11,10 +11,11 @@
    * so we have the format "preTextNormal [pretransformed pt1 <buttonText> preTransformed pt2|postTransformed](handle) postTextNormal"
    * Which read preExtension : preTextNormal + pretransformed pt1 + buttonText + preTransformed pt2 + postTextNormal
    * And after extension : preTextNormal + postTransformed + postTextNormal
+   * en fait let's do [[preTr1 <buttonText> preTr2|handle]]
    */
   const expansionMap = {
-    text1: `Si vous êtes dans une journée dans lesquels les détours sont autorisés, et si de surcroît ça pique votre curiosité, vous pouvez aller observer [<le very moment >|le very moment ](very_moment) où cette métaphore a surgit des bois pour venir me ravir de de sa savoureuse et croustillante composition dans cette fitéo ([<accent alsacien>|](accent_alsacien))`,
-    very_moment: `(c'est quand même truculent cette façon dont les anglophones utilisent l'adverbe "very" pour dire "le moment précis, le moment même", imaginez si on disait "le très moment", bref)`,
+    text1: `Si vous êtes dans une journée dans lesquels les détours sont autorisés, et si de surcroît ça pique votre curiosité, vous pouvez aller observer [[<le very moment>|very_moment]] où cette métaphore a surgit des bois pour venir me ravir de de sa savoureuse et croustillante composition dans cette fitéo ([[<accent alsacien>|accent_alsacien]])`,
+    very_moment: `le very moment (c'est quand même truculent cette façon dont les anglophones utilisent l'adverbe "very" pour dire "le moment précis, le moment même", imaginez si on disait "le très moment", bref)`,
     accent_alsacien: `ah oui je prends beaucoup de plaisir en ce moment à jouer inopinément au jeu de parler avec l'accent alsacien, il faut peut-être le savoir pour ne pas être trop déconcerté·e`
   }
 
@@ -23,4 +24,19 @@
   const expand = () => {
     console.log("coucou")
   }
+
+/* const testString = "partie un [[extrait 1 pre <link1> extrait 1 post|handle1]] partie deux [[extrait 2 pre <link2> extrait 2 post|handle2]] partie trois"
+  const regex = /\[\[(.*?)\<(.*?)\>(.*?)\|(.*?)\]\]/g
+  const matches = testString.matchAll(regex)
+  let matchArray = Array.from(matches)
+  for (let match of matches){
+    console.log("match", match)
+    //matchArray.value[index] = match
+  }
+  matchArray.forEach((match, index) => {
+    console.log("match "+index, match)
+  }) */
+  //console.log("matchArray", matchArray)
+
+
 </script>
--- components/TextModule.vue
+++ components/TextModule.vue
@@ -1,51 +1,67 @@
+
 <template>
-  <span>{{ preTextNormal }}</span>
-  <span v-if="!expanded">{{ pretransformed1 }}</span>
-  <span v-if="!expanded" class="text-blue-500 hover:bg-purple-500 hover:text-white cursor-pointer" @click="expand">{{ buttonText }}</span>
-  <span v-if="!expanded">{{ pretransformed2 }}</span>
-  <span v-if="expanded">{{ postTransformed }}</span>
-  <TextModule v-if="expanded" :handle="textHandle"></TextModule>
-  <span v-if="expanded" class="text-blue-500 hover:bg-purple-500 hover:text-white cursor-pointer" @click="foldBack">¬</span>
-  <span>{{ postTextNormal }}</span>
+  <span v-for="(contentBit, index) in content">
+    <span>{{ contentBit.whateverBefore }}</span>
+    <span v-if="!contentBit.expanded">{{ contentBit.preTransformed1 }}</span>
+    <span v-if="!contentBit.expanded" class="text-blue-500 hover:bg-purple-500 hover:text-white cursor-pointer" @click="expand(index)">{{ contentBit.buttonText }}</span>
+    <span v-if="!contentBit.expanded">{{ contentBit.preTransformed2 }}</span>
+    <TextModule v-if="contentBit.expanded" :handle="contentBit.textHandle"></TextModule>
+    <span v-if="contentBit.expanded" class="text-blue-500 hover:bg-purple-500 hover:text-white cursor-pointer" @click="foldBack(index)">¬</span>
+    <span>{{ contentBit.whateverAfter }}</span>
+  </span>
+  <!-- <pre>{{ content }}</pre> -->
 </template>
 
 <script setup>
   const { handle } = defineProps({
     handle: String,
   })
+  console.log("handle", handle)
   const expansionMap = inject("expansionMap")
-  const preTextNormal = ref(expansionMap[handle])
-  const expansionStruct = ref("")
-  const pretransformed1 = ref("")
-  const buttonText = ref("")
-  const pretransformed2 = ref("")
-  const postTransformed = ref("")
-  const textHandle = ref("")
-  const postTextNormal = ref("")
+  const matchArray = ref([])
+  const content = ref([])
 
-  const expanded = ref(false)
+  const regex = /((?:.|\n)*?)\[\[(.*?)\<(.*?)\>(.*?)\|(.*?)\]\]/gm // matches  "whatever before [[preTextNormal <pretransformed1> buttonText|textHandle]]" multiple times (but not whatever after)
+  const matches = expansionMap[handle].matchAll(regex)
+  const lastBitRegex = /(?:(?:.|\n)*)\]\](.*)/m // matches "whatever after", just for the last match
+  const lastBitMatches = expansionMap[handle].match(lastBitRegex)
 
-  
-  const regex = /(.*)\[(.*)<(.*)>(.*)\|(.*)\]\((.*?)\)(.*)/
-  const matches = expansionMap[handle].match(regex)
-  if (matches) {
-    ;[expansionStruct.value, preTextNormal.value, pretransformed1.value, buttonText.value, pretransformed2.value, postTransformed.value, textHandle.value, postTextNormal.value] = matches
-    console.log("expansionStruct", expansionStruct.value)
-    console.log("pretransformed1", pretransformed1.value)
-    console.log("buttonText", buttonText.value)
-    console.log("pretransformed2", pretransformed2.value)
-    console.log("postTransformed", postTransformed.value)
-    console.log("textHandle", textHandle.value)
-    console.log("postTextNormal", postTextNormal.value)
+  matchArray.value = Array.from(matches)
+  if (matchArray.value.length === 0) {
+    content.value.push({
+      whateverBefore: expansionMap[handle],
+      preTransformed1: "",
+      buttonText: "",
+      preTransformed2: "",
+      textHandle: "",
+      whateverAfter: "",
+      expanded: false,
+    })
+  }
+  matchArray.value.forEach((match, index) => {
+    content.value.push({
+      whateverBefore: match[1],
+      preTransformed1: match[2],
+      buttonText: match[3],
+      preTransformed2: match[4],
+      textHandle: match[5],
+      whateverAfter: "",
+      expanded: false,
+    })
+    if (index === matchArray.value.length - 1) {
+      content.value[index].whateverAfter = lastBitMatches[1]
+    }
+    //console.log("match " + index, match)
+    //console.log("match[2]", match[2])
+  })
+
+  const expand = (index) => {
+    content.value[index].expanded = true
+    console.log("coucou expand " + index)
   }
 
-  const expand = () => {
-    expanded.value = true
-    console.log("coucou expand")
-  }
-
-  const foldBack = () => {
-    expanded.value = false
-    console.log("coucou foldBack")
+  const foldBack = (index) => {
+    content.value[index].expanded = false
+    console.log("coucou foldBack " + index)
   }
 </script>
