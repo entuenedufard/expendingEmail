3c6c4f435e3713fa5a9b817262c3ce87e7794a8b
entuenedufard <entuenedufard@gmail.com>
2023-12-11 17:27:17 +0100

    Refactor TextModule component to display module content (without links or recursivity)

--- app.vue
+++ app.vue
@@ -1,31 +1,13 @@
 <template>
   <div>
     <div class="max-w-4xl p-10 pt-16 mx-auto">
-      <!-- <TextModule handle="text1"></TextModule > -->
-      <pre>{{ ao_modules }}</pre>
+      <TextModule handle="initial-block"></TextModule>
+      <!-- <pre>{{ ao_modules }}</pre> -->
     </div>
   </div>
 </template>
 
 <script setup>
-  /**
-   * so we have the format "preTextNormal [pretransformed pt1 <buttonText> preTransformed pt2|postTransformed](handle) postTextNormal"
-   * Which read preExtension : preTextNormal + pretransformed pt1 + buttonText + preTransformed pt2 + postTextNormal
-   * And after extension : preTextNormal + postTransformed + postTextNormal
-   * en fait let's do [[preTr1 <buttonText> preTr2|handle]]
-   */
-  const expansionMap = {
-    text1: `Si vous êtes dans une journée dans lesquels les détours sont autorisés, et si de surcroît ça pique votre curiosité, vous pouvez aller observer [[<le very moment>|very_moment]] où cette métaphore a surgit des bois pour venir me ravir de de sa savoureuse et croustillante composition dans cette fitéo ([[<accent alsacien>|accent_alsacien]])`,
-    very_moment: `le very moment (c'est quand même truculent cette façon dont les anglophones utilisent l'adverbe "very" pour dire "le moment précis, le moment même", imaginez si on disait "le très moment"), bref donc je disais, le very moment`,
-    accent_alsacien: `ah oui je prends beaucoup de plaisir en ce moment à jouer inopinément au jeu de parler avec l'accent alsacien, il faut peut-être le savoir pour ne pas être trop déconcerté·e. [[J'ai <une petite liste> ici si ça vous intéresse|liste_accent_alsacien]]`,
-    liste_accent_alsacien: `Il fa falloir chancher la pouteille te kas. 
-Ékoute, çut, che te l'ai técha tit au moins touçe fois !
-Che fais téforer ce papa au rhum.
-Che rekarte ça au plus fite et che refiens fers fous.`,
-  }
-
-  provide("expansionMap", expansionMap)
-
   const runtimeConfig = useRuntimeConfig()
   const apiBaseUrl = runtimeConfig.public.apiBaseUrl
 
@@ -50,4 +32,5 @@ Che rekarte ça au plus fite et che refiens fers fous.`,
       return rawValue.data
     },
   })
+  provide("ao_modules", ao_modules._rawValue)
 </script>
--- components/TextModule.vue
+++ components/TextModule.vue
@@ -1,5 +1,11 @@
 <template>
-  <span class="opacity-100" :class="{ 'duration-700': transitionValue, 'opacity-100': transitionValue }" v-for="(contentBit, index) in content">
+  <span v-for="block in moduleContent">
+    <span v-if="block.__component == 'associative-ontologies.normal-text-block'">{{ block.text }}</span>
+    <span v-if="block.__component == 'associative-ontologies.expandable-text-block'">{{ block.pre_button }}</span>
+    <span v-if="block.__component == 'associative-ontologies.expandable-text-block'">{{ block.button }}</span>
+    <span v-if="block.__component == 'associative-ontologies.expandable-text-block'">{{ block.post_button }}</span>
+  </span>
+  <!-- <span class="opacity-100" :class="{ 'duration-700': transitionValue, 'opacity-100': transitionValue }" v-for="(contentBit, index) in content">
     <span>{{ contentBit.whateverBefore }}</span>
     <span v-if="!contentBit.expanded">{{ contentBit.preTransformed1 }}</span>
     <span v-if="!contentBit.expanded" class="text-blue-500 hover:bg-purple-500 hover:text-white cursor-pointer" @click="expand(index)">{{ contentBit.buttonText }}</span>
@@ -7,8 +13,8 @@
     <TextModule class="opacity-50" v-if="contentBit.expanded" :handle="contentBit.textHandle"></TextModule>
     <span v-if="contentBit.expanded" class="text-blue-500 hover:bg-purple-500 hover:text-white cursor-pointer" @click="foldBack(index)">¬</span>
     <span>{{ contentBit.whateverAfter }}</span>
-  </span>
-  <!-- <pre>{{ content }}</pre> -->
+  </span> -->
+  <pre>{{ moduleContent }}</pre>
 </template>
 
 <script setup>
@@ -16,17 +22,25 @@
     handle: String,
   })
   console.log("handle", handle)
-  const expansionMap = inject("expansionMap")
-  const matchArray = ref([])
-  const content = ref([])
-  const transitionValue = ref(true)
+  const ao_modules = inject("ao_modules")
+  let module = ao_modules.filter((module) => {
+    return module.slug_handle === handle
+  })
+  let moduleContent = "no module found for handle " + handle
+  if (module.length === 0) {
+    console.log("no module found for handle " + handle)
+  } else moduleContent = module[0].content
 
-  const regex = /((?:.|\n)*?)\[\[(.*?)\<(.*?)\>(.*?)\|(.*?)\]\]/gm // matches  "whatever before [[preTextNormal <pretransformed1> buttonText|textHandle]]" multiple times (but not whatever after)
+  /* const matchArray = ref([])
+  const content = ref([])
+  const transitionValue = ref(true) */
+
+  /* const regex = /((?:.|\n)*?)\[\[(.*?)\<(.*?)\>(.*?)\|(.*?)\]\]/gm // matches  "whatever before [[preTextNormal <pretransformed1> buttonText|textHandle]]" multiple times (but not whatever after)
   const matches = expansionMap[handle].matchAll(regex)
   const lastBitRegex = /(?:(?:.|\n)*)\]\](.*)/m // matches "whatever after", just for the last match
-  const lastBitMatches = expansionMap[handle].match(lastBitRegex)
+  const lastBitMatches = expansionMap[handle].match(lastBitRegex) */
 
-  matchArray.value = Array.from(matches)
+  /* matchArray.value = Array.from(matches)
   if (matchArray.value.length === 0) {
     content.value.push({
       whateverBefore: expansionMap[handle],
@@ -37,8 +51,8 @@
       whateverAfter: "",
       expanded: false,
     })
-  }
-  matchArray.value.forEach((match, index) => {
+  } */
+  /* matchArray.value.forEach((match, index) => {
     content.value.push({
       whateverBefore: match[1],
       preTransformed1: match[2],
@@ -53,9 +67,9 @@
     }
     //console.log("match " + index, match)
     //console.log("match[2]", match[2])
-  })
+  }) */
 
-  const expand = (index) => {
+  /* const expand = (index) => {
     content.value[index].expanded = true
     console.log("coucou expand " + index)
   }
@@ -63,5 +77,5 @@
   const foldBack = (index) => {
     content.value[index].expanded = false
     console.log("coucou foldBack " + index)
-  }
+  } */
 </script>
